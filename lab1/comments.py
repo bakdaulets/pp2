75ea85a9fa0aaee341a0c6c5a325ceca7c8b398a
bakdaulets <b_tanirbergenov@kbtu.kz>
2024-01-22 15:19:37 +0600

    first

---
+++ lab1/comments.py
@@ -0,0 +1,27 @@
+'''
+
+Comments can be used to explain Python code.
+
+Comments can be used to make the code more readable.
+
+Comments can be used to prevent execution when testing code.
+'''
+#This is a comment
+print("Hello, World!")
+
+print("Hello, World!") #This is a comment
+
+#print("Hello, World!")
+print("Hello, World!")
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
